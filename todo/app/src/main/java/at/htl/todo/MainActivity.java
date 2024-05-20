ce1fa05e09fb13dc14bf75462620624a008ce18d
CaoAnton <antoncao1510@gmail.com>
2024-05-20 14:50:12 +0200

    fix: add permission to use the internet

--- todo/app/src/main/java/at/htl/todo/MainActivity.java
+++ todo/app/src/main/java/at/htl/todo/MainActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 
 import androidx.activity.ComponentActivity;
 import javax.inject.Inject;
+
+import at.htl.todo.model.TodoService;
 import at.htl.todo.ui.layout.MainView;
 import at.htl.todo.util.Config;
 import dagger.hilt.android.AndroidEntryPoint;
@@ -14,6 +16,9 @@ import dagger.hilt.android.AndroidEntryPoint;
 @AndroidEntryPoint
 public class MainActivity extends ComponentActivity {
 
+    static final String TAG = MainActivity.class.getSimpleName();
+    @Inject
+    TodoService todoService;
     @Inject
     MainView mainView;
 
@@ -22,6 +27,9 @@ public class MainActivity extends ComponentActivity {
         super.onCreate(savedInstanceState);
         Config.load(this);
         var base_url = Config.getProperty("json.placeholder.baseurl");
+
+        todoService.getAll();
+
         Log.i(TAG, "onCreate: " + base_url);
         mainView.buildContent(this);
     }
